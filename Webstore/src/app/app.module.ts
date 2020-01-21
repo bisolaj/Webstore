1093c9076ec11086766e92ccb4868ab7f554bb59
Code <code@COCTL-41288.local>
2020-01-21 14:44:34 -0800

    First Commit

---
+++ Webstore/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './views/pages/header/header.component';
+import { MainComponent } from './views/pages/main/main.component';
+import { FooterComponent } from './views/pages/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
