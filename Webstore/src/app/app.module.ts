87f6dab757994afffe65483aa6485c7ae0aa952b
Code <code@COCTL-41288.local>
2020-01-22 09:58:02 -0800

    Fifth Commit

--- Webstore/src/app/app.module.ts
+++ Webstore/src/app/app.module.ts
@@ -1,12 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './views/pages/header/header.component';
 import { MainComponent } from './views/pages/main/main.component';
 import { FooterComponent } from './views/pages/footer/footer.component';
 import { HomeComponent } from './views/pages/home/home.component';
+import { AboutusComponent } from './views/pages/aboutus/aboutus.component';
+import { FaqsComponent } from './views/pages/faqs/faqs.component';
+import { PageNotFoundComponent } from './views/pages/page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'aboutus',      component: AboutusComponent },
+  {
+    path: 'faqs',
+    component: FaqsComponent,
+    data: { title: 'Heroes List' }
+  },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -14,11 +32,15 @@ import { HomeComponent } from './views/pages/home/home.component';
     HeaderComponent,
     MainComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AboutusComponent,
+    FaqsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
