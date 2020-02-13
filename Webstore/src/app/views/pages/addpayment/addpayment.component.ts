39bf09a64c7724b9328805ba7217bfe15f1c0480
bisolaj <bisolaj@yahoo.com>
2020-02-12 17:45:46 -0800

    TwentySixth Commit

--- Webstore/src/app/views/pages/addpayment/addpayment.component.ts
+++ Webstore/src/app/views/pages/addpayment/addpayment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-addpayment',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addpayment.component.scss']
 })
 export class AddpaymentComponent implements OnInit {
+  addpayment: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.addpayment = this.formBuilder.group({
+      cnumber: ['', Validators.required],
+      ccode: ['', [Validators.required, Validators.minLength(3)]],
+      edate: ['', Validators.required],
+      
+    });
+    
   }
+
+  submitForm() {
+    if (this.addpayment.invalid) {
+      alert('You must entered all required information!');
+    }
+    else {
+      alert('Successful');
+      console.log(this.addpayment.value);
+      this.addpayment.reset();
+
+    }
+  }
+
 
 }
